6bf115b579830ed7c69ff6e0b2479bc2821ad30f
u31403 <u31403@P31403>
2023-12-05 11:22:09 +1000

    Добавьте файлы проекта.

---
+++ MathEquation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuadraticEquation_StasIvan
+{
+    public class MathEquation
+    {
+        public static double Disc(double a)
+        {
+            return a;
+        }
+    }
+}
